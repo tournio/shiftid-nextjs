9ae3925d3e475e10a19b93016f67e82fc4275f07
Scott Stebleton <scott@stebleton.net>
2025-01-04 18:02:15 -0500

    deprecation warnings should be silenced

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+  },
 }
 
 module.exports = nextConfig
